9ec77e53debfd914a870f59646c8ca7e84df95d7
Rohit Singh <singhrohit8111@gmail.com>
2023-10-25 12:55:21 +0530

    created backend file

---
+++ backend.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const axios = require("axios");
+const bodyParser = require("body-parser");
+const app = express();
+// const port = 3000; // Set your desired port
+const port = 'http://localhost:3000/';
+const cors = require("cors"); // Import the cors middleware
+
+const admin = require("firebase-admin");
+const getmessage = require("firebase-admin/messaging")
+
+const serviceAccount = require("./chatapp-b7a1e-firebase-adminsdk-mgozs-9481dd023f.json");
+app.use(cors()); // Use cors middleware
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+});
+
+// const messaging = getMessaging;
+
+console.log(getmessage, "getmessage");
+
+app.use(bodyParser.json());
+
+// Define an API route to send push notifications
+app.post("/send-notification", async (req, res) => {
+  try {
+    // Get registration tokens and notification data from the request
+    const { registrationTokens, data, notification, tokenToExclude } = req.body;
+
+    const filteredtoken = registrationTokens.filter((token) => token !== tokenToExclude);
+    console.log(filteredtoken, "filteredtoken");
+
+    const message = {
+      data: data, // Notification data (e.g., title, body)
+      tokens: filteredtoken, // Array of registration tokens
+      notification: notification
+    };
+    console.log(req.body, "req.body");
+    // Send a push notification using the axios library or Firebase Admin SDK
+    getmessage.getMessaging().sendEachForMulticast(message)
+      .then((response) => {
+        console.log(response + ' messages were sent successfully');
+      });
+    // Replace this with your FCM or custom notification logic
+
+    // Respond with success or an error message
+    res.json({ message: "Push notification sent successfully" });
+  } catch (error) {
+    console.log(error, "error");
+    res.status(500).json({ error: "Error sending push notification" });
+  }
+});
+
+app.listen(3000, () => {
+  console.log(`Server is running on port ${3000}`);
+});
